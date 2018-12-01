46a066a902b1d3981a4d643a591b3161d4f19311
ruchadaporn.s <ruchadaporn.s@gmail.com>
2018-12-01 18:21:00 +0700

    add list

--- listQue.js
+++ listQue.js
@@ -2,32 +2,24 @@ module.exports = {
    listQueReq: function(event) {
    		msg = {
             "type": "template",
-            "altText": "Change List",
+            "altText": "this is a buttons template",
             "template": {
-                "type": "carousel",
-                "columns": [
-                    {
-                        "title": "List Change",
-                        "actions": [
-                            {
-                                "type": "postback",
-                                "label": "CR 1",
-                                "data": "action=approved&cr=1"
-                            },
-                            {
-                                "type": "postback",
-                                "label": "CR 2",
-                               "data": "action=approved&cr=1"
-                            },
-                            {
-                                "type": "postback",
-                                "label": "CR 3",
-                                "data": "action=approved&cr=1"
-                            }
-                        ]
-                    }
-                   
-                ]
+                "type": "buttons",
+                "thumbnailImageUrl": "https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100",
+                "title": "Change List",
+                "actions": [{
+                    "type": "postback",
+                    "label": "CR 1",
+                    "data": "action=approve&cr=1"
+                }, {
+                    "type": "postback",
+                    "label": "CR 2",
+                    "data": "action=approve&cr=2"
+                }, {
+                    "type": "postback",
+                    "label": "CR 2",
+                    "data": "action=approve&cr=3"
+                }]
             }
         }
     
