ecc0d2089f3d304815ffe98f05b375ae8b78addb
witawas <44538129+witawas@users.noreply.github.com>
2018-12-01 23:42:28 +0700

    4444

--- usrReqQue.js
+++ usrReqQue.js
@@ -6,11 +6,16 @@ module.exports = {
      var x;
      return con.connect( function(err) {
         if (err) throw err;
-         let result = con.query("SELECT queue FROM `capdata` WHERE status = 2 order by queue desc limit 1", function (err, result, fields) {
-          if (err) throw err; 
-          console.log(result); 
-          console.log(result[0].queue);
-          return result[0].queue;
+         let result = un         
+         con.query("SELECT queue FROM `capdata` WHERE status = 2 order by queue desc limit 1", function (err, result, fields) {
+          //if (err) throw err; 
+          //console.log(result); 
+          //console.log(result[0].queue);
+          //return result[0].queue;
+          if (err) 
+              callback(err,null);
+          else
+              callback(null,result[0].queue);
          // x= result[0].queue;
         });
         console.log('should be result here', result);
