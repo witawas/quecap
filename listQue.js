937309e5e46aa1d0296debcebbb875a8a61f650a
ruchadaporn.s <ruchadaporn.s@gmail.com>
2018-12-01 18:16:21 +0700

    list change

--- listQue.js
+++ listQue.js
@@ -7,49 +7,26 @@ module.exports = {
                 "type": "carousel",
                 "columns": [
                     {
-                        "thumbnailImageUrl": "https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100",
-                        "title": "CR 1",
-                        "text": "เพิ่ม Disk",
+                        "title": "List Change",
                         "actions": [
                             {
                                 "type": "postback",
-                                "label": "Confirm",
-                                "data": "action=confirm&cr=1"
+                                "label": "CR 1",
+                                "data": "action=approved&cr=1"
                             },
                             {
                                 "type": "postback",
-                                "label": "Approved",
+                                "label": "CR 2",
                                "data": "action=approved&cr=1"
                             },
                             {
                                 "type": "postback",
-                                "label": "Done",
-                                "data": "action=done&cr=1"
-                            }
-                        ]
-                    },
-                    {
-                        "thumbnailImageUrl": "https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100",
-                        "title": "CR 2",
-                        "text": "เพิ่ม Disk",
-                        "actions": [
-                            {
-                                "type": "postback",
-                                "label": "Confirm",
-                                "data": "action=confirm&cr=2"
-                            },
-                            {
-                                "type": "postback",
-                                "label": "Approved",
-                               "data": "action=approved&cr=2"
-                            },
-                            {
-                                "type": "postback",
-                                "label": "Done",
-                                "data": "action=done&cr=2"
+                                "label": "CR 3",
+                                "data": "action=approved&cr=1"
                             }
                         ]
                     }
+                   
                 ]
             }
         }
