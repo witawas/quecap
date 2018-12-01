2070c9a58a7aeee51d60d9711ff2359a96268dea
ruchadaporn.s <ruchadaporn.s@gmail.com>
2018-12-01 18:40:29 +0700

    fix bug

--- listQue.js
+++ listQue.js
@@ -7,18 +7,18 @@ module.exports = {
                 "type": "buttons",
                 "thumbnailImageUrl": "https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100",
                 "title": "Change List",
-                "text": "",
+                "text": "List All",
                 "actions": [{
                     "type": "postback",
-                    "label": "CR 1",
+                    "label": "CR1",
                     "data": "action=approve&id=1"
                 }, {
                     "type": "postback",
-                    "label": "CR 2",
+                    "label": "CR2",
                     "data": "action=approve&id=2"
                 }, {
                     "type": "postback",
-                    "label": "CR 2",
+                    "label": "CR2",
                     "data": "action=approve&id=3"
                 }]
             }
