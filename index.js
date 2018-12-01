9b69f5e937deb84492faff7ae08e4158df475ceb
witawas <44538129+witawas@users.noreply.github.com>
2018-12-01 21:15:27 +0700

    remove connection

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ const myQue = require('./myQue');
 const setQue = require('./setQue');
 const nodemailer = require('nodemailer');
 
-var mysql = require('mysql');
-var con = mysql.createConnection({
-  host: "119.59.120.32",
-  user: "gooruapp_queue",
-  password: "GFhPccLkV4",
-  database: "gooruapp_queue"
-});
+//var mysql = require('mysql');
+//var con = mysql.createConnection({
+///  host: "119.59.120.32",
+//  user: "gooruapp_queue",
+//  password: "GFhPccLkV4",
+//  database: "gooruapp_queue"
+//});
 
 // const  = rerquie('./fiel')
 
