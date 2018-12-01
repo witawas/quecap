ae3556db32d39f1eebefdda8db5e77e463766aff
witawas <44538129+witawas@users.noreply.github.com>
2018-12-01 21:19:02 +0700

    upadte code check message

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ function handleEvent(event) {
 
   
   
-  if (event.type !== 'message' || event.message.type !== 'text' || event.type !== 'postback') {
+  if (event.type !== 'message' || event.message.type !== 'text') {
     // ignore non-text-message event
     return Promise.resolve(null);
   }
