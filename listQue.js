7826cd24a3ab0f36286c9228cadf056d33f3241e
ruchadaporn.s <ruchadaporn.s@gmail.com>
2018-12-01 18:23:23 +0700

    fix bug

--- listQue.js
+++ listQue.js
@@ -6,23 +6,23 @@ module.exports = {
             "template": {
                 "type": "buttons",
                 "thumbnailImageUrl": "https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100",
-                "title": "Change List",
+                "title": "Menu",
+                "text": "Please select",
                 "actions": [{
                     "type": "postback",
-                    "label": "CR 1",
-                    "data": "action=approve&cr=1"
+                    "label": "Buy",
+                    "data": "action=buy&itemid=123"
                 }, {
                     "type": "postback",
-                    "label": "CR 2",
-                    "data": "action=approve&cr=2"
+                    "label": "Add to cart",
+                    "data": "action=add&itemid=123"
                 }, {
-                    "type": "postback",
-                    "label": "CR 2",
-                    "data": "action=approve&cr=3"
+                    "type": "uri",
+                    "label": "View detail",
+                    "uri": "http://example.com/page/123"
                 }]
             }
         }
-    
      	return msg;
    }
 }
