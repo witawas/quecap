1d4b8b5f6b09f5fd96fe7ed5a9e694b7a5908236
witawas <44538129+witawas@users.noreply.github.com>
2018-12-02 00:33:50 +0700

    4545

--- usrReqQue.js
+++ usrReqQue.js
@@ -7,7 +7,7 @@ module.exports = {
      return con.connect( function(err,callback) {
         if (err) throw err;
          con.query("SELECT queue FROM `capdata` WHERE status = 2 order by queue desc limit 1", function (err, result, fields) {
-          //if (err) throw err; 
+          //if (err) throw err;  
           //console.log(result); 
           //console.log(result[0].queue);
           //return result[0].queue;
