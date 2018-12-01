3af2a2a5789c2bcbd7767a40c39950488d07a75a
witawas <44538129+witawas@users.noreply.github.com>
2018-12-01 23:08:51 +0700

    rrr

--- usrReqQue.js
+++ usrReqQue.js
@@ -4,9 +4,9 @@ module.exports = {
   getQue: async function(event,con) {
      //return 'file usrReqQue';
      var x;
-     return await con.connect(function(err) {
+     return await con.connect(await function(err) {
         if (err) throw err;
-        await con.query("SELECT queue FROM `capdata` WHERE status = 2 order by queue desc limit 1", await function (err, result, fields) {
+         con.query("SELECT queue FROM `capdata` WHERE status = 2 order by queue desc limit 1", await function (err, result, fields) {
           if (err) throw err;
           console.log(result);
           console.log(result[0].queue);
