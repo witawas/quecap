e2dd84bab68cd98e16f5a8f7de606a16c1dea6a1
witawas <44538129+witawas@users.noreply.github.com>
2018-12-01 22:54:33 +0700

    xxx

--- usrReqQue.js
+++ usrReqQue.js
@@ -1,11 +1,7 @@
 module.exports = {
-  myCallback: function(err, data) {
-    if (err) throw err; // Check for the error and throw if it exists.
-    console.log('got data: '+data); // Otherwise proceed as usual.
-    return data;
-  } ,
+
   
-  async getQue: function(event,con) {
+  getQue: async function(event,con) {
      //return 'file usrReqQue';
      var x;
      return con.connect(function(err) {
