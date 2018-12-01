78d0690ae1f03afee05aeb894fa5573e9c2466bf
ruchadaporn.s <ruchadaporn.s@gmail.com>
2018-12-01 18:58:02 +0700

    list

--- listQue.js
+++ listQue.js
@@ -7,17 +7,18 @@ module.exports = {
                 "type": "buttons",
                 "thumbnailImageUrl": "https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100",
                 "title": "Change List",
+                "text": "List All",
                 "actions": [{
                     "type": "postback",
-                    "label": "CR1",
+                    "label": "CR 1",
                     "data": "action=approve&id=1"
                 }, {
                     "type": "postback",
-                    "label": "CR2",
+                    "label": "CR 2",
                     "data": "action=approve&id=2"
                 }, {
                     "type": "postback",
-                    "label": "CR2",
+                    "label": "CR 2",
                     "data": "action=approve&id=3"
                 }]
             }
