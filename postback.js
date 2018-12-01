ce1cc60bfe75a9ee2b187b8892bbfa28f5edeb6c
ruchadaporn.s <ruchadaporn.s@gmail.com>
2018-12-01 23:19:15 +0700

    fix bug

--- postback.js
+++ postback.js
@@ -1,14 +1,38 @@
 module.exports = {
    handle_postback: function(event,con) {
         //Update queue,cap date where capdate and queue is null and status = 1
-        //var sql = "select capdata.id from capdata inner join changeinfo on capdata.changeNo = changeinfo.changeNo where capdata.status = '1' order by changeinfo.deploydate,capdata.reqdate";
-        
+        var sql = "select capdata.id as id from capdata inner join changeinfo on capdata.changeNo = changeinfo.changeNo where capdata.status = '1' order by changeinfo.deploydate,capdata.reqdate";
+        var msg;
+        con.query(sql, function (err, result, fields) 
+        {
+          if (err) throw err;
+          var test = result;
+          var length = Object.keys(result).length;
+          console.log(   length   );
+
+          for (var i = 0; i < length; i++) 
+          {
+
+            //console.log(result[i].id);
+                msg =  { type: 'text', text: 'send notification success!! : '+result[i].id};
+                return msg;
+          };
+
+        });
+
+
+
+
+
+
+
+
+
+
+
 
         msg =  { type: 'text', text: 'send notification success!! '};
-        //return msg;
-        //data: 'action=approve&id=3'
-        //var q = uri.queryKey['q'];
-
+        
         return msg;
    }
 
