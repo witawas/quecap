8b0302435af0c8e57e40e87dbd9962138dabb1db
witawas <44538129+witawas@users.noreply.github.com>
2018-12-02 01:42:33 +0700

    44444

--- index.js
+++ index.js
@@ -191,9 +191,9 @@ async function Booking(data,userId, callback){
 
 async function checkQueNow(event, callback){
   con.query("SELECT queue FROM `capdata` WHERE capdate = CURRENT_DATE and status = 2 order by queue desc limit 1';", function (err, result, fields) {
-    
+    console.log('------->>>>>result checkQueNow', result)
     if(result === undefined){
-        return callback('ตอนยังไม่ถึงเวลา CAP จ้า');
+        return callback('ตอนนี้ยังไม่ถึงเวลา CAP จ้า');
     }
     console.log('result checkQueNow', result[0].queue)
     return callback(result[0].queue);
