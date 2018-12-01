a84527d739d364b50ddcc3475c1c289fb73e1295
witawas <44538129+witawas@users.noreply.github.com>
2018-12-01 22:19:20 +0700

    XXX

--- usrReqQue.js
+++ usrReqQue.js
@@ -1,15 +1,18 @@
 module.exports = {
    getQue: function(event,con) {
      //return 'file usrReqQue';
+     var x;
      con.connect(function(err) {
         if (err) throw err;
         con.query("SELECT queue FROM `capdata` WHERE status = 2 order by queue desc limit 1", function (err, result, fields) {
           if (err) throw err;
           console.log(result);
           console.log(result[0].queue);
-          return result[0].queue;
+          //return result[0].queue;
+          x= result[0].queue;
         });
       });
       
+      return x;
    }
 }
